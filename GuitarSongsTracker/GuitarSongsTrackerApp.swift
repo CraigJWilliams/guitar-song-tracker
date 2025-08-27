313ddc4249c0bbe059328f97bb5d8d3d1194e36d
CraigJWilliams <craig@craigjwilliams.co.uk>
2025-08-27 21:18:31 +0100

    Initial Commit

---
+++ GuitarSongsTracker/GuitarSongsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarSongsTrackerApp.swift
+//  GuitarSongsTracker
+//
+//  Created by Craig Williams on 27/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuitarSongsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuitarSongsTrackerTests/GuitarSongsTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarSongsTrackerTests.swift
+//  GuitarSongsTrackerTests
+//
+//  Created by Craig Williams on 27/08/2025.
+//
+
+import Testing
+@testable import GuitarSongsTracker
+
+struct GuitarSongsTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
